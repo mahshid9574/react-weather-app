7e851524b8d021308ba2aab0e0a607a222f9260e
mahshid9574 <84104563+mahshid9574@users.noreply.github.com>
2024-11-07 13:13:26 +0330

    installed axios

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
+import React from "react";
+import Weather from "./Weather";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <h1>Mahshid's Weather App</h1>
-      <footer>
-        This project is coded by{" "}
-        <a href="https://github.com/mahshid9574">Mahshid Jamali</a> and is open
-        soucrced on{" "}
-        <a href="https://github.com/mahshid9574/react-weather-app">Github</a>
-      </footer>
+      <div className="container">
+        <Weather defaultCity="New York" />
+
+        <footer>
+          This project is coded by{" "}
+          <a href="https://github.com/mahshid9574">Mahshid Jamali</a>, open
+          soucrced on{" "}
+          <a href="https://github.com/mahshid9574/react-weather-app">Github</a>,
+          and hosted on{" "}
+          <a href="https://react-weather-app-coral-three.vercel.app/">Vercel</a>
+        </footer>
+      </div>
     </div>
   );
 }
